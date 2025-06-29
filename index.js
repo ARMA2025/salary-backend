85a28ee4ae002c47be36ac36af54e3fea5c9e47e
ARMA2025 <araibia.mohamed@gmail.com>
2025-06-29 15:18:46 +0100

    fix bug 2

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
-const mysql = require('mysql2');
 const cors = require('cors');
+const mysql = require('mysql2');
 const app = express();
 
 app.use(cors());
@@ -38,5 +38,5 @@ app.post('/api/salaries', (req, res) => {
 });
 
 app.listen(5000, () => {
-  console.log('Server is running on port 5000');
+  console.log('Server running on port 5000');
 });
